3b96e702c04746fbfde3838698537bd0350617dd
ganyiwatakunda <118890097+ganyiwatakunda@users.noreply.github.com>
2024-05-03 11:32:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
 import streamlit as st 
 
+class ContentBasedRecommender:
+    def __init__(self, matrix):
+        self.matrix_similar = matrix
+
+    def _print_message(self, song, recom_song):
+        rec_items = len(recom_song)
+        
+        print(f'The {rec_items} recommended songs for {song} are:')
+        for i in range(rec_items):
+            print(f"Number {i+1}:")
+            print(f"{recom_song[i][1]} by {recom_song[i][2]} with {round(recom_song[i][0], 3)} similarity score") 
+            print("--------------------")
+        
+    def recommend(self, recommendation):
+        # Get song to find recommendations for
+        song = recommendation['song']
+        # Get number of songs to recommend
+        number_songs = recommendation['number_songs']
+        # Get the number of songs most similars from matrix similarities
+        recom_song = self.matrix_similar[song][:number_songs]
+        # print each item
+        self._print_message(song=song, recom_song=recom_song)
+
 def main():
     st.title("Music Recommendation System")
 
