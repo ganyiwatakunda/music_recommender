af42e35b510bf11c8df80a75c4ba46dfba4fe945
ganyiwatakunda <118890097+ganyiwatakunda@users.noreply.github.com>
2024-05-14 09:26:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
+
+from typing import List, Dict
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Load the dataset
+
 songs = pd.read_csv('content based recommedation system/songdata.csv')
+
+songs.head()
+
+
+
+songs = songs.sample(n=5000).drop('link', axis=1).reset_index(drop=True)
+
+
 
 songs['text'] = songs['text'].str.replace(r'\n', '')
 
-# Create TF-IDF matrix
+
+
 tfidf = TfidfVectorizer(analyzer='word', stop_words='english')
+
 lyrics_matrix = tfidf.fit_transform(songs['text'])
 
 
-# Compute cosine similarity matrix
 cosine_similarities = cosine_similarity(lyrics_matrix)
+
+
+
 similarities = {}
 
 for i in range(len(cosine_similarities)):
@@ -24,8 +38,9 @@ for i in range(len(cosine_similarities)):
     # After that, we'll store in similarities each name of the 50 most similar songs.
     # Except the first one that is the same song.
     similarities[songs['song'].iloc[i]] = [(cosine_similarities[i][x], songs['song'][x], songs['artist'][x]) for x in similar_indices][1:]
-    
-# Content-based recommender class
+
+
+
 class ContentBasedRecommender:
     def __init__(self, matrix):
         self.matrix_similar = matrix
@@ -33,7 +48,7 @@ class ContentBasedRecommender:
     def _print_message(self, song, recom_song):
         rec_items = len(recom_song)
 
-        print(f'The {rec_items} recommended songs for {song} are:')
+        print(f'The {rec_items} recommended song for {song} are:')
         for i in range(rec_items):
             print(f"Number {i+1}:")
             print(f"{recom_song[i][1]} by {recom_song[i][2]} with {round(recom_song[i][0], 3)} similarity score")
@@ -42,29 +57,48 @@ class ContentBasedRecommender:
     def recommend(self, recommendation):
         # Get song to find recommendations for
         song = recommendation['song']
-        # Get number of songs to recommend
-        number_songs = recommendation['number_songs']
-        # Get the number of songs most similars from matrix similarities
-        recom_song = self.matrix_similar[song][:number_songs]
-        # print each item
-        self._print_message(song=song, recom_song=recom_song)
+
+
+
 
 recommedations = ContentBasedRecommender(similarities)
-# Streamlit app
+
+
+recommendation = {
+    "song": songs['song'].iloc[10],
+}
+
+recommedations.recommend(recommendation)
+
+
+
+import streamlit as st
+
 def main():
     st.title("Music Recommendation System")
 
     # Sidebar input
-    song_title = st.sidebar.text_input("Enter a song title")
-    number_songs = st.sidebar.slider("Number of songs to recommend", min_value=1, max_value=10, value=5)
+    song_title = st.text_input("Enter a song title")
+    
 
-    if st.sidebar.button("Recommend"):
+    if st.button("Recommend"):
+        # Get the index of the song in the DataFrame
+        ###song_index = song[song['song'] == song_title].index[0]##
         # Create an instance of the ContentBasedRecommender class
         recommender = ContentBasedRecommender(cosine_similarities)
         # Define the recommendation input
-        recommendation = {'song': song_title, 'number_songs': number_songs}
+        recommendation = {'song': song_title}
         # Get recommendations
         recommendations = recommender.recommend(recommendation)
+               
+        # Display recommendations
+        st.write(f"The recommended songs for {song_title} are:")
+        for recommendations in songs['song']:
+            st.write('Recommendations:', recommendations)
+            #st.write(f"Number {i+1}:")
+            #st.write(f"{rec_song[1]} by {rec_song[2]} with {round(rec_song[0], 3)} similarity score")
+            st.write("--------------------")
+
 
 if __name__ == '__main__':
     main()
